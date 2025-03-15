fde95ee5876dbcc68f9e27bc55a8cb29b4d24e54
voofoo <116034746+voofoo@users.noreply.github.com>
2025-03-15 18:42:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const statusDiv = document.getElementById("status");
 const changeUI = (status = "acquired") => {
     const isActive = status === "acquired";
     wakeButton.dataset.status = isActive ? "on" : "off";
-    wakeButton.textContent = `Αφυπνιση ${isActive ? "ΑΝΕΝΕΡΓΗ" : "ΕΝΕΡΓΗ"}`;
+    wakeButton.textContent = `Αφυπνιση ${isActive ? "ΕΝΕΡΓΗ" : "ΑΝΕΝΕΡΓΗ"}`;
     statusElem.textContent = `Η λειτουργια αφυπνισης ${isActive ? "ενεργοποιήθηκε!" : "απενεργοποιήθηκε."}`;
 
     // Update status container styles
